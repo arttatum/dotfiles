d727ef377c0e49f019f73bbeb123d6f6078d64d6
Will L <woughney1@gmail.com>
2024-01-09 21:20:02 +0000

    Move which-key config to separate folder

---
+++ nvim/lua/plugins/config/which-key/init.lua
@@ -0,0 +1,28 @@
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+local wk = require("which-key")
+wk.setup({
+	plugins = {
+		spelling = true,
+		presets = {
+			operators = true, -- adds help for operators like d, y, ...
+			motions = true, -- adds help for motions
+			text_objects = true, -- help for text objects triggered after entering an operator
+			windows = true, -- default bindings on <c-w>
+			nav = true, -- misc bindings to work with windows
+			z = true, -- bindings for folds, spelling and others prefixed with z
+			g = true, -- bindings for prefixed with g
+		},
+	},
+	key_labels = { ["<leader>"] = "SPC" },
+})
+wk.register({
+	mode = { "n", "v" },
+	["<leader>d"] = { name = "+debugger" },
+	["<leader>f"] = { name = "+find" },
+	["<leader>g"] = { name = "+git" },
+	["<leader>q"] = { name = "+quit/session" },
+	["<leader>t"] = { name = "+toggle" },
+	["<leader>v"] = { name = "+lsp" },
+	["<leader><tab>"] = { name = "+tabs" },
+})
--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -22,40 +22,7 @@ require("packer").startup(function(use)
 	use("stevearc/dressing.nvim")
 
 	-- Which Key?
-
-	use({
-		"folke/which-key.nvim",
-		config = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-			local wk = require("which-key")
-			wk.setup({
-				plugins = {
-					spelling = true,
-					presets = {
-						operators = true, -- adds help for operators like d, y, ...
-						motions = true, -- adds help for motions
-						text_objects = true, -- help for text objects triggered after entering an operator
-						windows = true, -- default bindings on <c-w>
-						nav = true, -- misc bindings to work with windows
-						z = true, -- bindings for folds, spelling and others prefixed with z
-						g = true, -- bindings for prefixed with g
-					},
-				},
-				key_labels = { ["<leader>"] = "SPC" },
-			})
-			wk.register({
-				mode = { "n", "v" },
-				["<leader>d"] = { name = "+debugger" },
-				["<leader>f"] = { name = "+find" },
-				["<leader>g"] = { name = "+git" },
-				["<leader>q"] = { name = "+quit/session" },
-				["<leader>t"] = { name = "+toggle" },
-				["<leader>v"] = { name = "+lsp" },
-				["<leader><tab>"] = { name = "+tabs" },
-			})
-		end,
-	})
+	use("folke/which-key.nvim")
 
 	-- Add symbols to LSP suggestions
 	use("onsails/lspkind.nvim")
