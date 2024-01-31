c23d384c3acdbbb8d2aecfb0dec7b93b373a6f77
Will Loughney <wloughne@amazon.com>
2024-01-31 14:31:29 +0000

    Update harpoon config

--- nvim/lua/plugins/config/harpoon/init.lua
+++ nvim/lua/plugins/config/harpoon/init.lua
@@ -1,6 +1,32 @@
-require("harpoon").setup()
+local harpoon = require("harpoon")
+harpoon:setup()
+
+-- basic telescope configuration
+local conf = require("telescope.config").values
+
+local function toggle_telescope(harpoon_files)
+	local file_paths = {}
+	for _, item in ipairs(harpoon_files.items) do
+		table.insert(file_paths, item.value)
+	end
+
+	require("telescope.pickers")
+		.new({}, {
+			prompt_title = "Harpoon",
+			finder = require("telescope.finders").new_table({
+				results = file_paths,
+			}),
+			previewer = conf.file_previewer({}),
+			sorter = conf.generic_sorter({}),
+		})
+		:find()
+end
+
+vim.keymap.set("n", "<leader>fm", function()
+	toggle_telescope(harpoon:list())
+end, { desc = "Open harpoon window" })
 
 -- <leader>fm via telescope for ui
 vim.keymap.set("n", "<leader>m", function()
-	require("harpoon.mark").add_file()
+	harpoon:list():append()
 end, { desc = "Make harpoon" })
--- nvim/lua/plugins/config/telescope/init.lua
+++ nvim/lua/plugins/config/telescope/init.lua
@@ -7,7 +7,3 @@ vim.keymap.set("n", "<leader>fgc", builtin.git_commits, { desc = "Search commits
 vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Grep" })
 vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Search recent" })
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Search keymaps" })
-
-local telescope = require("telescope")
-telescope.load_extension("harpoon")
-vim.keymap.set("n", "<leader>fm", telescope.extensions.harpoon.marks, { desc = "Search harpoons" })
