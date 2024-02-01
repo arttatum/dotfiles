4d7553a326e21fcca3b700b1f2c241f9722da883
Will Loughney <wloughne@amazon.com>
2024-02-01 16:44:18 +0000

    Prettify breakpoints, folds in status column

--- nvim/lua/plugins/config/dap/init.lua
+++ nvim/lua/plugins/config/dap/init.lua
@@ -44,3 +44,21 @@ dap.listeners.before.event_exited.dapui_config = function()
 end
 
 require("nvim-dap-virtual-text").setup()
+
+vim.fn.sign_define(
+	"DapBreakpoint",
+	{ text = "󱜙", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapBreakpointCondition",
+	{ text = "󱚟", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapBreakpointRejected",
+	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapLogPoint",
+	{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+)
+vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -138,8 +138,30 @@ require("packer").startup(function(use)
 	})
 
 	-- folds
-	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
+	use({
+		"kevinhwang91/nvim-ufo",
+		requires = {
+			{ "kevinhwang91/promise-async" },
+			{
+				"luukvbaal/statuscol.nvim",
+			},
+		},
+	})
 
+	use({
+		"luukvbaal/statuscol.nvim",
+		config = function()
+			local builtin = require("statuscol.builtin")
+			require("statuscol").setup({
+				relculright = true,
+				segments = {
+					{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+					{ text = { "%s" }, click = "v:lua.ScSa" },
+					{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+				},
+			})
+		end,
+	})
 	-- refactoring
 	use({
 		"ThePrimeagen/refactoring.nvim",
